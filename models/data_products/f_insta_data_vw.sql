dc700e301fc1fdb46b1f08e31a29da149b7c3afb
arkham7638 <dakshsharma810@hotmail.com>
2025-02-02 18:34:19 +0000

    Pushing the code to github

---
+++ models/data_products/f_insta_data_vw.sql
@@ -0,0 +1,20 @@
+WITH BASE AS (
+    SELECT
+        FROM_HOME, 
+        CAPTION, 
+        SHARES, 
+        FROM_EXPLORE, 
+        FROM_OTHER, 
+        IMPRESSIONS, 
+        LIKES, 
+        FOLLOWS, 
+        HASHTAGS, 
+        FROM_HASHTAGS, 
+        COMMENTS, 
+        PROFILE_VISITS, 
+        SAVES, 
+        _FIVETRAN_DELETED AS IS_DELETED,
+        _FIVETRAN_SYNCED AS SNF_UPDATE_DATE
+    FROM {{ source('insta','insta_data') }}
+)
+SELECT * FROM BASE
